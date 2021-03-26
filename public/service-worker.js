56b6c33295059febcbeed7645701e4f7a7b12e44
Kristin Saunders <kmsaunders7@gmail.com>
2021-03-26 09:57:06 -0500

    create manifest, start on service-worker

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/assets/css/style.css',
+    '/assets/images/icon-192x192.png',
+    '/assets/images/icon-512x512.png',
+    '/assets/js/index.js',
+    '/assets/js/indexedDB.js',
+    'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+  ];
